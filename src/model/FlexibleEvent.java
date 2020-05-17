79dd78048321d4854c71d613faa566e45f248b4c
Jimmy Rowland <tian_yu@student.smc.edu>
2020-05-17 13:51:57 -0700

    init3

--- src/model/FlexibleEvent.java
+++ src/model/FlexibleEvent.java
@@ -1,8 +1,19 @@
 package model;
 
+import java.util.Calendar;
+
 public class FlexibleEvent implements Event {
     int duration;
+    Calendar start;
+    Calendar end;
     public FlexibleEvent(String name, String location, String description, int duration){
+
+    }
+
+    void setStart(){
+
+    }
+    void setEnd(){
 
     }
 }

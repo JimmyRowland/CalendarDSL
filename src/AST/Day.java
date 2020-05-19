4f39bc28031b614396059c8c028a8a4f5a5e9fa1
w6j1b <avery.devlin@hotmail.ca>
2020-05-19 16:58:22 -0700

    Created Validator class & methods for validating ast nodes

--- src/AST/Day.java
+++ src/AST/Day.java
@@ -3,14 +3,13 @@ package AST;
 import model.io.Tokenizer;
 
 public class Day implements ASTnode {
-    String day = null;
+    String day;
 
     @Override
     public void parse() {
         Tokenizer t = Tokenizer.getTokenizer();
         String token = t.checkNext();
         day = Validator.validateDay(token);
-        day = token;
     }
 
     @Override

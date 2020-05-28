f855485c6bc86e9f8336c3b2d89c875988a21fc6
Jimmy Rowland <tian_yu@student.smc.edu>
2020-05-27 17:26:16 -0700

    Main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,6 +2,10 @@ package ui;
 
 
 import libs.Tokenizer;
+import model.Event;
+import model.Scheduler;
+import model.io.Parser;
+import model.io.Writer;
 
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
@@ -10,11 +14,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-//        System.out.println("hello");
-//     List<String> literals = Arrays.asList(";", "new calendar", "new event", "event end",
-//                "group:", "<", ">", "(", ",", ")", "|", "start", "finish", "location:", "repeat:",
-//                "daily", "every", "priority", "description:", "@", "from", "to");
-//    Tokenizer.makeTokenizer("src/test/AST/test1",literals);
+        Parser parse = new Parser(args[0]);
+        if(args.length == 0){
+            System.out.println("usage: main path_to_input_file");
+            return;
+        }
+        try{
+            Scheduler scheduler = parse.calendar();
+            scheduler.allocateFlexibleEvents();
+            String[] pathList = args[0].split("/");
+            String name = pathList[pathList.length-1] + ".cvs";
+            Writer.write(name,scheduler);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
